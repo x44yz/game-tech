29bf50ef6855f7fabba4a8c2381aeac583221d60
twenty0ne <twenty0ne@qq.com>
2018-10-19 19:13:52 +0800

    [Ability]update

--- Ability/Assets/Scripts/Ability/AbilityTargeter.cs
+++ Ability/Assets/Scripts/Ability/AbilityTargeter.cs
@@ -7,6 +7,22 @@ using UnityEngine;
 // 释放一个技能，会创建一个技能目标选择器
 public class AbilityTargeter
 {
+	public List<Actor> targets = new List<Actor>();
+
+	public Actor target
+	{
+		get
+		{
+			if (targets.Count == 1)
+				return targets[0];
+			else
+			{
+				Debug.LogError("target only called when targets.Count == 1");
+				return null;
+			}
+		}
+	}
+
     public virtual void Activate()
     {
     }
