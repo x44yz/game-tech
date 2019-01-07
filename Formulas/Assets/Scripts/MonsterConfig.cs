b2159cc24e58307d287553c7352e8e03aee96341
twenty0ne <twenty0ne@qq.com>
2019-01-08 07:18:07 +0800

    [Formulas]update monster config

--- Formulas/Assets/Scripts/MonsterConfig.cs
+++ Formulas/Assets/Scripts/MonsterConfig.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +8,8 @@ public class CfgMonster
 	public int lvl;
 	public int exp;
 	public int[] hp = new int[2];
-	public int speed;	// move speed
+	public int walkSpeed;
+	public int runSpeed;
 	// tc / rune 都是掉落
 	public int tc;	// treasure class
 	public string rune; // 符文
@@ -27,7 +28,7 @@ public static class MonsterConfigs
 		cfg.lvl = 4;
 		cfg.exp = 22;
 		cfg.hp[0] = 2; cfg.hp[1] = 6;
-		cfg.speed = 4;
+		cfg.walkSpeed = 4;
 		cfg.attack[0] = 1; cfg.attack[1] = 2;
 		cfg.attackRating = 23;
 
@@ -35,5 +36,49 @@ public static class MonsterConfigs
 		cfg.name = "Blood Hawk";
 		cfg.lvl = 6;
 		cfg.exp = 29;
+		cfg.hp[0] = 2; cfg.hp[1] = 6;
+		cfg.walkSpeed = 4;
+		cfg.attack[0] = 2; cfg.attack[1] = 3;
+		cfg.attackRating = 41;
+
+		//
+		cfg = new CfgMonster();
+		cfg.name = "Fallen";
+		cfg.lvl = 1;
+		cfg.exp = 18;
+		cfg.hp[0] = 1; cfg.hp[1] = 4;
+		cfg.walkSpeed = 5;
+		cfg.attack[0] = 1; cfg.attack[1] = 2;
+		cfg.attackRating = 8;
+
+		cfg = new CfgMonster();
+		cfg.name = "Carver";
+		cfg.lvl = 5;
+		cfg.exp = 42;
+		cfg.hp[0] = 4; cfg.hp[1] = 9;
+		cfg.walkSpeed = 5;
+		cfg.attack[0] = 2; cfg.attack[1] = 4;
+		cfg.attackRating = 31;
+
+		//
+		cfg = new CfgMonster();
+		cfg.name = "Dark Hunter";
+		cfg.lvl = 2;
+		cfg.exp = 31;
+		cfg.hp[0] = 5; cfg.hp[1] = 9;
+		cfg.walkSpeed = 5;
+		cfg.runSpeed = 5;
+		cfg.attack[0] = 1; cfg.attack[1] = 3;
+		cfg.attackRating = 12;
+
+		cfg = new CfgMonster();
+		cfg.name = "Vile Hunter";
+		cfg.lvl = 5;
+		cfg.exp = 54;
+		cfg.hp[0] = 10; cfg.hp[1] = 17;
+		cfg.walkSpeed = 6;
+		cfg.runSpeed = 8;
+		cfg.attack[0] = 2; cfg.attack[1] = 5;
+		cfg.attackRating = 31;
 	}
 }
