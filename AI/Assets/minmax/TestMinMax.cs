f75c93e938cb1839b28bc214b177d0c8b9936abd
twenty0ne <twenty0ne@qq.com>
2019-03-28 00:26:12 +0800

    [AI]tic-tac-toe

--- AI/Assets/minmax/TestMinMax.cs
+++ AI/Assets/minmax/TestMinMax.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TestMinMax : MonoBehaviour
 {
+	public const float BOARD_WIDTH = 108f;
 
+	public Transform tfBoardCell0x0;
+
+	public GameObject objO = null;
+	public GameObject objX = null;
+
+	private void Update() 
+	{
+		
+	}
+
+	public void OnClickBoardCell(int cidx)
+	{
+		Debug.Log("xx-- OnClickBoardCell > " + cidx);
+	}
 }
