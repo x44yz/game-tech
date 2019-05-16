ff793dbcf7e06671ff7f487d643c1692590a5b50
twenty0ne <twenty0ne@qq.com>
2019-05-17 07:11:34 +0800

    Formulas

--- Formulas/Assets/Scripts/KingdomRush/KRConfigs.cs
+++ Formulas/Assets/Scripts/KingdomRush/KRConfigs.cs
@@ -15,6 +15,7 @@ public class KRCfgTower
 public static class KRConfigs
 {
 	public static KRCfgTower[] towers = new KRCfgTower[]{
-			new KRCfgTower(){id="archer", name="Archer Tower", level=1, buildCost=70, minDamage=4, maxDamage=6, attackRate=0.8, scope=280 },
+			new KRCfgTower(){id="archer", name="Archer Tower", level=1, buildCost=70, minDamage=4, maxDamage=6, attackRate=0.8f, scope=280 },
+			new KRCfgTower(){id="marksmen", name="Marksmen Tower", level=2, buildCost=110, minDamage=7, maxDamage=11, attackRate=0.6f, scope=280 },
 		};
 }
