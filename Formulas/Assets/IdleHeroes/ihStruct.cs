8ccd424f148576b543191f76e4e26abc65389370
twenty0ne <twenty0ne@qq.com>
2019-10-16 09:40:18 +0800

    [Formulas]idle heroes update

---
+++ Formulas/Assets/IdleHeroes/ihStruct.cs
@@ -0,0 +1,23 @@
+namespace IdleHeroes 
+{
+	public enum Faction
+	{
+		Abyss,
+	}
+
+	public enum Role
+	{
+		Priest,
+	}
+
+	public class ihHeroStruct
+	{
+		public Faction faction;
+		public Role role;
+		public int power;
+		public int health;
+		public int attack;
+		public int armor;
+		public int speed;
+	}
+}
---
+++ Formulas/Raw/idle-heroes/README.md
@@ -0,0 +1,4 @@
+https://idleheroes.fandom.com/wiki/Norma
+https://idleheroes.fandom.com/wiki/Tower_of_Oblivion_Layer_1-100
+https://playidleheroes.com/
+
