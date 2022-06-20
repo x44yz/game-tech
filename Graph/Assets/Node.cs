659317f922f49b72b34b02d15022614ffa6e6a54
k021Ng <k021Ng@qq.com>
2022-06-20 22:04:00 +0800

    [Graph]add

---
+++ Graph/Assets/Node.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int id;
+    // public float x;
+    // public float y;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, Vector3.one * 0.1f);
+    }
+}
