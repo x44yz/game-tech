53885cc1f43f6c01fd311fec61a137b514d9a20c
k021Ng <k021Ng@qq.com>
2022-10-17 10:26:12 +0800

    [AI]pursue

--- AI/Assets/Pursue/Pursue.cs
+++ AI/Assets/Pursue/Pursue.cs
@@ -8,23 +8,38 @@ using UnityEngine;
 public class Pursue : AIBehavoir
 {
     public Transform target;
+    public float maxPrediction;
 
     [Header("RUNTIME")]
     public AIAgent agent;
+    public AIAgent targetAgent;
 
     // [Header("DEBUG")]
 
     private void Awake() 
     {
         agent = GetComponent<AIAgent>();
+        targetAgent = target.GetComponent<AIAgent>();
     }
 
     void Update()
     {
-        Vector3 dist = Utils.Vector3ZeroY(target.position - agent.pos);
-        Vector3 accel = dist.normalized * agent.maxAccel;
+        Vector3 dir = Utils.Vector3ZeroY(targetAgent.pos - agent.pos);
+        float dist = dir.magnitude;
+        float speed = agent.velocity.magnitude;
+        float prediction;
+        // 预测值，其实是在 target 的方向上偏移
+        // 当两者距离近的时候，预测值就小，反之越大
+        if (speed <= dist / maxPrediction)
+            prediction = maxPrediction;
+        else
+            prediction = dist / speed;
 
-        agent.accel = accel;
+        Vector3 targetPos = targetAgent.pos + targetAgent.velocity * prediction;
+
+        // 以下与 seek 相同
+        dir = Utils.Vector3ZeroY(targetPos - agent.pos);
+        agent.accel = dir.normalized * agent.maxAccel;
     }
 
     void OnDrawGizmos()
